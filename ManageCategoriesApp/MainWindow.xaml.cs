f3a55ed3c3b3a6b7fe9b431b459f7431c70b0555
tanlnguyen-fpt <tannlde150056@fpt.edu.vn>
2022-09-26 22:45:27 +0700

    Add project files.

---
+++ ManageCategoriesApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace ManageCategoriesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ManageCategories categories = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadCategory();
+        }
+
+        private void LoadCategory()
+        {
+            lvCategories.ItemsSource = categories.GetCategories();
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Category category = new() { CategoryName = txtCategoryName.Text };
+            categories.InsertCategory(category);
+            LoadCategory();
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Category category = new() { CategoryName = txtCategoryName.Text, CategoryID = int.Parse(txtCategoryID.Text) };
+            categories.UpdateCategory(category);
+            LoadCategory();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Category category = new() { CategoryID = int.Parse(txtCategoryID.Text) };
+            categories.DeleteCategory(category);
+            LoadCategory();
+        }
+    }
+}
---
+++ ManageCategoriesApp/ManageCategories.cs
@@ -0,0 +1,108 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+
+namespace ManageCategoriesApp
+{
+    public record Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+    public class ManageCategories
+    {
+        string connString = "Host=localhost:5432;Username=postgres;Password=postgres;Database=MyStore";
+        NpgsqlConnection conn;
+
+        public List<Category> GetCategories()
+        {
+            List<Category> categories = new();
+            conn = new(connString);
+            try
+            {
+                conn.Open();
+                NpgsqlCommand cmd = new("Select CategoryId, CategoryName from Categories", conn);
+                NpgsqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        categories.Add(new Category { CategoryID = reader.GetInt32(0), CategoryName = reader.GetString(1) });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return categories;
+        }
+
+        public void InsertCategory(Category category)
+        {
+            try
+            {
+                conn.Open();
+                NpgsqlCommand cmd = new("Insert into Categories(categoryname) values (@CategoryName)", conn);
+                cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            try
+            {
+                conn.Open();
+                NpgsqlCommand cmd = new("Update Categories set categoryname=@CategoryName where CategoryId=@CategoryId", conn);
+                cmd.Parameters.AddWithValue("@CategoryId", category.CategoryID);
+                cmd.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            try
+            {
+                conn.Open();
+                NpgsqlCommand cmd = new("Delete from Categories where CategoryId=@CategoryId", conn);
+                cmd.Parameters.AddWithValue("@CategoryId", category.CategoryID);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
